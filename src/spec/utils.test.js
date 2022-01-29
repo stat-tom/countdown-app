b78a3a773fa8cbd3f24b480da42add0a22947603
Tomasz Statkowski <43075475+stat-tom@users.noreply.github.com>
2022-01-29 14:29:08 +0100

    Update utils.test.js

--- src/spec/utils.test.js
+++ src/spec/utils.test.js
@@ -4,14 +4,18 @@ test("3600 is 01:00:00", () => {
     expect(secondsToHourMinuteSecond(3600)).toBe("01:00:00");
 });
 
-test("0 is 00:00:00", () => {
-    expect(secondsToHourMinuteSecond(0)).toBe("00:00:00");
+test("1 is 00:00:01", () => {
+    expect(secondsToHourMinuteSecond(1)).toBe("00:00:01");
 });
   
-  test("3601 is 01:00:01", () => {
-    expect(secondsToHourMinuteSecond(3601)).toBe("01:00:01");
+  test("86400 is 24:00:00", () => {
+    expect(secondsToHourMinuteSecond(86400)).toBe("24:00:00");
 });
 
 test("Name invalid if its length is 0", () => {
     expect(isValidName(0)).toBe(false);
+});
+
+test("Name invalid if its length is greater than 128", () => {
+    expect(isValidName(129)).toBe(false);
 });
